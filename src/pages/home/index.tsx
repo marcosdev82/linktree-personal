36acafeabbc8fb0093afcf4f076920bd99ce2e9d
marcosdev82 <marcostavares.dev@hotmail.com>
2026-07-18 16:49:49 -0300

    feat: componentização das Social Media

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,28 +7,28 @@ export function Home() {
                     <img className="rounded-full" src="https://avatars.githubusercontent.com/u/10318700?v=4" alt="Avatar" /> 
                 </div>
                 <h1 className="md:text-4xl text-3xl font-black">@marcostv</h1>
-                <p className="text-lg text-gray-900 text-base text-center">Veja meus trabalhos e descubra como posso ajudar você. 🚀​</p>
+                <p className="text-lg text-gray-900 text-center">Veja meus trabalhos e descubra como posso ajudar você. 🚀​</p>
             </div>
 
             <main className="flex flex-col items-center justify-center gap-4 mt-8 w-full max-w-md"> 
                 <section className="w-full">
-                   <a href="https://github.com/marcostv" className="block w-full text-center bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition-colors select-none transition-transform hover:scale-105">Website</a>
+                           <a href="https://github.com/marcostv" className="block w-full text-center bg-[oklch(0.56_0.17_149.56)] text-white py-2 px-4 rounded-full hover:opacity-90 transition-all select-none hover:scale-105">Website</a>
                 </section>
 
                 <section className="w-full">
-                   <a href="https://www.linkedin.com/in/marcostv" className="block w-full text-center bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition-colors select-none transition-transform hover:scale-105">Portfolio</a>
+                           <a href="https://www.linkedin.com/in/marcostv" className="block w-full text-center bg-[oklch(0.56_0.17_149.56)] text-white py-2 px-4 rounded-full hover:opacity-90 transition-all select-none hover:scale-105">Portfolio</a>
                 </section>
 
                 <section className="w-full">
-                   <a href="https://github.com/marcostv" className="block w-full text-center bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition-colors select-none transition-transform hover:scale-105">GitHub</a>
+                           <a href="https://github.com/marcostv" className="block w-full text-center bg-[oklch(0.56_0.17_149.56)] text-white py-2 px-4 rounded-full hover:opacity-90 transition-all select-none hover:scale-105">GitHub</a>
                 </section>
 
                 <section className="w-full">
-                   <a href="https://github.com/marcostv" className="block w-full text-center bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition-colors select-none transition-transform hover:scale-105">Instagram</a>
+                           <a href="https://github.com/marcostv" className="block w-full text-center bg-[oklch(0.56_0.17_149.56)] text-white py-2 px-4 rounded-full hover:opacity-90 transition-all select-none hover:scale-105">Instagram</a>
                 </section>
 
                 <section className="w-full">
-                   <a href="https://github.com/marcostv" className="block w-full text-center bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition-colors select-none transition-transform hover:scale-105">Whatsapp</a>
+                           <a href="https://github.com/marcostv" className="block w-full text-center bg-[oklch(0.56_0.17_149.56)] text-white py-2 px-4 rounded-full hover:opacity-90 transition-all select-none hover:scale-105">Whatsapp</a>
                 </section>
 
             </main>
