e1d52713a8c43532dc1ea04587900fc2c291b6a3
marcosdev82 <marcostavares.dev@hotmail.com>
2026-07-18 16:51:55 -0300

    feat: adiciona atributos rel e target ao componente Social para melhorar a segurança

--- src/compents/Social/index.tsx
+++ src/compents/Social/index.tsx
@@ -9,6 +9,8 @@ export default function Social({ url, children }: SocialProps) {
     return (
         <a  
             href={url}
+            rel="noopener noreferrer"
+            target="_blank"
             className="flex items-center justify-center gap-2 text-center border border-[oklch(0.56_0.17_149.56)] text-[oklch(0.56_0.17_149.56)] w-9 h-9 rounded-full hover:opacity-90 transition-all select-none hover:scale-105"
             >
             {children} 
