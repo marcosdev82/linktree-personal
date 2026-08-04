95a3a9df39a18cce9d1b1ae803020c75a96deb6c
marcosdev82 <marcostavares.dev@hotmail.com>
2026-08-04 08:08:24 -0300

    fix: corrigir erro de digitação no nome da função useFlashMessage

--- src/hooks/useFlashMessage.js
+++ src/hooks/useFlashMessage.js
@@ -1,6 +1,6 @@
 import bus from '../utils/bus';
 
-export default function useFFlashMessage() {
+export default function useFlashMessage() {
     function setFlashMessage(msg, type) {
         bus.emit('flash', { message: msg, type: type });
     }
