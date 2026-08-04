84e9672bde8c2551fdce5af9be985212b8aa6235
marcosdev82 <marcostavares.dev@hotmail.com>
2026-08-04 11:59:27 -0300

    feat: implementar componente Message para exibir mensagens flash com tratamento de eventos

--- src/compents/Message/index.js
+++ src/compents/Message/index.js
@@ -1,3 +1,35 @@
-import { Message } from './index.jsx';
+import { useEffect, useState } from "react";
+import { bus } from "../../utils/bus";
+
+function Message() {
+
+    const [visible, setVisible] = useState(false)
+    const [type, setType] = useState('')
+    const [message, setMessage] = useState('')
+    
+    useEffect(() => {
+ 
+        bus.addEventListener("flash", ({message, type}) => {
+            setVisible(true)
+            setType(type)
+            setMessage(message)
+
+            setTimeout(() => {
+                setVisible(false)
+            }, 3000)
+        })
+        
+    }, [])
+
+    return (
+       visible && (
+        <div className={`message ${type}`}>
+            {message}
+        </div>
+       )
+    )
+
+
+}
 
 export default Message;
