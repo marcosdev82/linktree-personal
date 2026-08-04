fe49d401c7a61b910343a1a0e03fc5d838321131
marcosdev82 <marcostavares.dev@hotmail.com>
2026-08-04 16:56:08 -0300

    feat: integrar useFlashMessage ao useAuth para melhorar o feedback ao usuário durante o cadastro

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,19 +1,26 @@
 import auth from "../services/auth"
 
 import { useEffect, useState } from "react"
+import { useFlashMessage } from "./useFlashMessage"
 
 export default function useAuth() {
 
+    const { setFlashMessage } = useFlashMessage()
+
     async function register(user) {
+
+        let msgText = "Cadastro realizado com sucesso!"
+        let msgType = "success"
+
         try {
             const data = await auth.post("/users/register", user).then((res) => res.data)
-            
-            console.log(data)
         } catch (error) {
-            console.error("Erro ao registrar usuário:", error)
-            return false
+            msgText = error.response.data.message
+            msgType = "error"
         }
+
+         setFlashMessage(msgText, msgType)
     }
 
-    return { register }
+    return { setFlashMessage, register }
 }
